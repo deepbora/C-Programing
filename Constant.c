f01f13b69049d6a6bd426ef0ab3463fecf2c070e
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-04-08 17:45:15 +0530

    constant of c

---
+++ Constant.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Program to calculate average of 5 numbers entered by users.
+
+int main()
+{
+    const  int  a=10;
+   printf("%d\n",a);
+//   a=a+20;
+   printf("%d",a);
+
+
+    
+    return 0;
+}
