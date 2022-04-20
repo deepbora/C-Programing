e67bccff265971f2708c8a575aa5ef2bbf2be47e
Deepjyoti Bora <59271987+deepbora@users.noreply.github.com>
2022-04-20 15:53:58 +0530

    c range

---
+++ range.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    short int a=32767;
+    short int b=32768;
+    short int c=-32768;
+    short int d=-32769;
+    short int e=-32790;
+
+
+    printf("%d \n",a);
+    printf("%d \n",b);
+    printf("%d \n",c);
+    printf("%d \n",d);
+    printf("%d \n",e);
+
+
+
+
+    return 0;
+}
